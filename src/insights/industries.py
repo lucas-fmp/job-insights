f067fd4eebd123059668ab45be6c3a695b5a48d6
Lucas Fernando Macedo Pereira <lucas@pop-os.localdomain>
2023-02-20 11:42:37 -0300

    feat - requirement 3 - implement function get_unique_industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,17 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
+    list_of_dicts_of_jobs = read(path)
+    list_industries = []
 
-    Must call `read`
+    for job in list_of_dicts_of_jobs:
+        if job["industry"] not in list_industries:
+            if job["industry"] != "":
+                list_industries.append(job["industry"])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    return list_industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
