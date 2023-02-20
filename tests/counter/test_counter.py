ad99f417c1fea5bc12ecac75e431714d392c5075
Lucas Fernando Macedo Pereira <lucas@pop-os.localdomain>
2023-02-20 16:54:52 -0300

    test - requirement 10 - counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    word_count = count_ocurrences("data/jobs.csv", "Python")
+    assert word_count == 1639
