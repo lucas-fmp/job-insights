a64183ca452606e4660b2cebabff550563145820
Lucas Fernando Macedo Pereira <lucas@pop-os.localdomain>
2023-02-20 15:13:08 -0300

    feat - requirement 9 - implement function filter_by_salary_range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -46,18 +46,13 @@ def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
 def filter_by_salary_range(
     jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
+    filtered_jobs = []
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                filtered_jobs.append(job)
+        except (ValueError):
+            pass
 
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    return filtered_jobs
