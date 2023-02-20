e23736d26d8155a7ba4a6b41463d4fd8a371d500
Lucas Fernando Macedo Pereira <lucas@pop-os.localdomain>
2023-02-20 11:31:58 -0300

    feat - requirement 2 - implement function get_unique_job_types

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -11,21 +11,15 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
+    list_of_dicts_of_jobs = read(path)
+    list_of_job_types = []
 
-    Must call `read`
+    for job in list_of_dicts_of_jobs:
+        if job["job_type"] not in list_of_job_types:
+            if job["job_type"] != "":
+                list_of_job_types.append(job["job_type"])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    return list_of_job_types
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
